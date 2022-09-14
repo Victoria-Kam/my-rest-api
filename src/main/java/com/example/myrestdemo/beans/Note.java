cf908762f3ac7e45d2ae727aaf1ac686761be422
Victoria <for.git.159@gmail.com>
2022-09-14 20:42:20 +0300

    pre-alpha version again  my restAPI

--- src/main/java/com/example/myrestdemo/beans/Note.java
+++ src/main/java/com/example/myrestdemo/beans/Note.java
@@ -1,4 +1,12 @@
 package com.example.myrestdemo.beans;
 
+import java.time.format.DateTimeFormatter;
+
 public class Note {
+
+    private long idNote;
+    private String title;
+    private String content;
+    private DateTimeFormatter dateTime;
+    private long idAuthor;
 }
